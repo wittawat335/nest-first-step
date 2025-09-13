48d3fc8d9521ffed1234c9d8ea8348138e9e7652
Wittawat_F <wittawat.f@softdebut.com>
2025-09-13 12:28:51 +0700

    #

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    @Get()
+    getAll(){
+        return "product";
+    }
+}
