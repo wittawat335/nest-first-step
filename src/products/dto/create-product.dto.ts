b0540b82ccdfc2cf3010029d544c9de250b010ad
Wittawat_F <wittawat.f@softdebut.com>
2025-11-13 17:57:52 +0700

    #

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,11 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+  readonly name: string;
+  readonly description?: string;
+  readonly price: number;
+}
+
+export class UpdateProductDto {
+    readonly name?: string;
+    readonly description?: string;
+    readonly price?: number;
+  }
