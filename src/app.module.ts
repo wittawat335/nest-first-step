69c11a073ef26c8b30a7d5cc76e20fe2b7ece5e7
Wittawat_F <wittawat.f@softdebut.com>
2025-11-13 17:19:46 +0700

    update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`mongodb://localhost:27017`, {
+      user: 'root',
+      pass: 'P@ssw0rd2025',
+      dbName: 'nest-first-db',
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
