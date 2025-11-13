33f5075ff09be51699bb79885947fa117be1b6f5
Wittawat_F <wittawat.f@softdebut.com>
2025-11-13 17:54:50 +0700

    update service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -12,15 +12,15 @@ export class ProductsService {
     return `This action returns all products`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} product`;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  update(id: string, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} product`;
   } //
 }
